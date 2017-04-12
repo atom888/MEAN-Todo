d5c505052c9a923a60dd5af4060f66c49a8b40d9
Adam <adam.dipinto@gmail.com>
2017-04-12 03:21:43 +0000

    Setup engine for html to require ejs

--- server.js
+++ server.js
@@ -12,8 +12,10 @@ var app = express();
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+app.engine("html", require("ejs").renderFile);
 
 // Middleware
+app.use(express.static(path.join(__dirname, "client")));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
